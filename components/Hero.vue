df03db1a664adaeaa2473817d249fc19f86183f5
Romadhan Prasetyo <romadhanedy@gmail.com>
2020-10-29 13:47:30 +0700

    :zap: Update description on hero

--- components/Hero.vue
+++ components/Hero.vue
@@ -3,7 +3,7 @@
         <section>
             <div class="container mx-auto px-4">
                 <div class="gl-readable-line-length">
-                    <p class="text-base">Hello! I am <span class="font-bold">Romadhan Prasetyo</span>. I am a <span class="font-semibold">Web Developer</span>. My 💚 favorite <span class="font-semibold">programming language</span> are Javascript, PHP, GoLang, and Python. <span class="font-semibold">Working</span> from home during quarantine 🏡</p>
+                    <p class="text-base">Hello! I am <span class="font-bold">Romadhan Prasetyo</span>. I am a <span class="font-semibold">Web Developer</span> and creator of the open source <a href="https://github.com/dyprast/nuxt-stisla" target="blank" class="gl-link-color font-semibold">Nuxt Stisla</a>. My 💚 favorite <span class="font-semibold">programming language</span> are Javascript, PHP, GoLang, and Python. <span class="font-semibold">Working</span> from home during quarantine 🏡</p>
                     <br>
                     <p class="text-base">I also enjoy 🎸. Favorite song: <span class="font-semibold">Love Story - Taylor Swift 🎼</span></p>
                 </div>
