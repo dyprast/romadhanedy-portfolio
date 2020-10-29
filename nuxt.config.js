afa45e948b8334f752900f53ba62032d10ceb94d
Romadhan Prasetyo <romadhanedy@gmail.com>
2020-10-29 13:42:06 +0700

    :bookmark: Meta google search

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,8 +5,9 @@ export default {
     title: "Romadhan Prasetyo | Dyprast",
     meta: [
       { charset: "utf-8" },
+      { name: "google-site-verification", content: "w2tKgPWu24ur4xsvhTai56PK_2EU4nM5wLk0gy4QAe8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
-      { hid: "description", name: "description", content: "" }
+      { hid: "description", name: "description", content: "Web developer and creator of the open source Nuxt Stisla." }
     ],
     link: [
 			{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
