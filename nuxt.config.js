8953457185d838afabd02c69b71dc81b2a1050aa
Romadhan Prasetyo <romadhanedy@gmail.com>
2020-11-07 19:23:10 +0700

    Update nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,7 @@ export default {
   ssr: false,
   target: "static",
   head: {
-    title: "Romadhan Prasetyo | Dyprast",
+    title: "Romadhan Edy Prasetyo",
     meta: [
       { charset: "utf-8" },
       { name: "google-site-verification", content: "w2tKgPWu24ur4xsvhTai56PK_2EU4nM5wLk0gy4QAe8" },
