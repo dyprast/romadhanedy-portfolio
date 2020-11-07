e016a6ecf5800eb6b606952bcfd473f1a84ba141
Romadhan Prasetyo <romadhanedy@gmail.com>
2020-11-07 19:25:28 +0700

    :zap: Update store

--- store/index.js
+++ store/index.js
@@ -2,6 +2,27 @@ import portfolios from './api/portfolio'
 import skills from './api/skills'
 
 export const state = () => ({
+    pages: {
+        current_page: "",
+        title: "Romadhan Edy Prasetyo"
+    },
     portfolios,
-    skills
+    skills,
 })
+
+export const mutations = {
+    INITIAL_PAGE: (state, { page, title }) => {
+        state.pages.current_page = page
+        state.pages.title = title
+    }
+}
+
+export const actions = {
+    INITIAL_PAGE: async ({ commit }, { page, title }) => {
+        try {
+            commit("INITIAL_PAGE", { page: page, title: title })
+        } catch (ex) {
+            console.log(ex)
+        }
+    }
+}
